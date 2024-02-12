25a9a48de17b047ff47a63710f5be070e88fbd66
Sima <simakovand9@gmail.com>
2024-02-12 15:26:34 +0300

    eslint

--- rickandmorty/.eslintrc.cjs
+++ rickandmorty/.eslintrc.cjs
@@ -1,18 +1,33 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:import/recommended",
+    // "airbnb"
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh", "import"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"@typescript-eslint/no-unused-vars": "off",
+		"react/prop-types": "off",
+		"react/react-in-jsx-scope": "off",
+		"react/no-array-index-key": "off",
+		"jsx-a11y/anchor-is-valid": "off",
+		"jsx-a11y/label-has-associated-control": "off",
+		"jsx-a11y/no-static-element-interactions": "off",
+		"react/jsx-one-expression-per-line": "off",
+		"react/jsx-no-useless-fragment": "off",
+		"default-param-last": "off",
+		"no-param-reassign": ["error", { "props": false }],
+		"import/extensions": ["error", "ignorePackages", { "tsx": "always" }],
+		"indent": ["error", "tab"],
+		"quotes": ["error", "double"]
+	},
 }
